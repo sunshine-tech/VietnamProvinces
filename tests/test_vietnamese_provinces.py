a7b08b2dc95ef2ea93de7bae49239ddf83e937d8
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2020-03-01 15:18:48 +0700

    Add test cases

---
+++ tests/test_vietnamese_provinces.py
@@ -0,0 +1,15 @@
+from vietnam_provinces.base import Province
+
+
+def test_province_enum():
+    from vietnam_provinces.enums.districts import ProvinceEnum
+    assert isinstance(ProvinceEnum.KON_TUM.value, Province)
+    assert ProvinceEnum.BEN_TRE.value.name == 'Tỉnh Bến Tre'
+    assert ProvinceEnum.HO_CHI_MINH.value.name == 'Thành phố Hồ Chí Minh'
+    assert ProvinceEnum.HA_NOI.value.name == 'Thành phố Hà Nội'
+    assert ProvinceEnum.PHU_YEN.value.phone_code == 257
+
+
+def test_importable_ward_enum():
+    from vietnam_provinces.enums.wards import WardEnum
+    assert WardEnum.GL_CHROH_PONAN_24060.value.name == 'Xã Chrôh Pơnan'
--- vietnam_provinces/__init__.py
+++ vietnam_provinces/__init__.py
@@ -2,5 +2,7 @@ from pathlib import Path
 
 from single_version import get_version
 
+from .base import Province, District, Ward  # noqa
+
 
 __version__ = get_version('vietnamese_provinces', Path(__file__).parent.parent)
