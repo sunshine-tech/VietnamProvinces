3b038ad3d73226789edd305b97162c8aa9bd255a
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2025-08-03 15:46:21 +0700

    Try ability to dump Province as dict

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 from devtools import debug
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer
 
 from vietnam_provinces import Province, ProvinceCode
 
@@ -13,12 +13,36 @@ class Profile(BaseModel):
     address: Address
 
 
+class DAddress(BaseModel):
+    province: Province
+
+    @field_serializer('province')
+    def serialize_province(self, province: Province):
+        return province._asdict()
+
+
+class DProfile(BaseModel):
+    name: str
+    address: DAddress
+
+
 def test_build_from_dict():
     p = Province.from_code(ProvinceCode.P_79)
-    address = {'province': p}
+    address = {'province': p._asdict()}
     profile = Profile(name='Nguyễn Hồng Quân', address=address)
     debug(profile)
     assert profile.address.province.code == ProvinceCode.P_79
     d = profile.model_dump()
     debug(d)
     assert d['address']['province'] == p
+
+
+def test_dump_as_dict():
+    p = Province.from_code(ProvinceCode.P_79)
+    address = {'province': p}
+    profile = DProfile(name='Nguyễn Hồng Quân', address=address)
+    debug(profile)
+    assert profile.address.province.code == ProvinceCode.P_79
+    d = profile.model_dump()
+    debug(d)
+    assert d['address']['province'] == p._asdict()
